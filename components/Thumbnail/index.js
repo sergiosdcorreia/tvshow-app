c8907ccb4406415f19a6f1b080442e6db5aa7253
Sergio Correia <sergiosdcorreia@gmail.com>
2020-07-22 22:26:22 +0100

    Added styles to Thumbnail component

--- components/Thumbnail/index.js
+++ components/Thumbnail/index.js
@@ -1,10 +1,14 @@
-const Thumbnail = ({ imageUrl, caption }) => {
+import ThumbnailStyles from './styles';
+
+const Thumbnail = ({ imageUrl = 'https://via.placeholder.com/210x295?text=?', caption }) => {
     return (
         <div className="thumbnail">
-            <img src={imageUrl} className="Thumbnail__image" />
-            <h4 className="Thumbnail__caption">{caption}</h4>
+            <img src={imageUrl} className="thumbnail__image" />
+            <h4 className="thumbnail__caption">{caption}</h4>
+
+            <style jsx>{ThumbnailStyles}</style>
         </div>
-    )
-}
+    );
+};
 
 export default Thumbnail;
--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -9,7 +9,7 @@ const Home = ({ shows }) => {
 
             return (
                 <li key={index}>
-                    <Thumbnail imageUrl={show.image.medium} caption={show.name} />
+                    <Thumbnail imageUrl={(show.image && show.image.medium) || undefined } caption={show.name} />
                 </li>
             )
         })
