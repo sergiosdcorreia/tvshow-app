1dd6046f21d05857c362805dec1bc530f4c86ad6
Sergio Correia <sergiosdcorreia@gmail.com>
2020-07-23 19:38:35 +0100

    Added Cast component

--- pages/[country]/[showId].js
+++ pages/[country]/[showId].js
@@ -1,8 +1,9 @@
 import axios from "axios";
 import parse from 'html-react-parser';
+import Cast from '../../components/Cast';
 
 const ShowDetails = ({ show }) => {
-    const { name, image, summary } = show;
+    const { name, image, summary, _embedded } = show;
 
     return (
         <div className="show-details">
@@ -14,6 +15,8 @@ const ShowDetails = ({ show }) => {
             <h1>{name}</h1>
             {parse(summary)}
 
+            {_embedded.cast.length > 0 && <Cast cast={_embedded.cast} />}
+
             <style jsx>{`
                 .show-details__poster {
                     height: 200px;
@@ -24,12 +27,16 @@ const ShowDetails = ({ show }) => {
     );
 };
 
-ShowDetails.getInitialProps = async () => {
-    const response = await axios.get('http://api.tvmaze.com/shows/1?embed=cast')
+ShowDetails.getInitialProps = async ({ query }) => {
+    const { showId } = query;
+
+    const response = await axios.get(
+        `http://api.tvmaze.com/shows/${showId}?embed=cast`
+    );
 
     return {
         show: response.data
-    }
-}
+    };
+};
 
 export default ShowDetails;
